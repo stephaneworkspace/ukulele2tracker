59af485c366e9876dacb0c2a92f58580d05611e0
Stéphane Bressani <s.bressani@bluewin.ch>
2019-09-27 21:25:19 +0200

    initial avec modification xcode 9 (10 ne fonctionne pas sans modifier AppDelegate.swif)

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+// coded for X Code 9 -> for 10 and upper : https://stackoverflow.com/questions/50794391/ios-app-xcode-build-errors
