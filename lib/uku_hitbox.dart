96228796d96bc295ebd84fe2574e1248feb0588f
Stéphane Bressani <s.bressani@bluewin.ch>
2019-09-30 18:11:46 +0200

    backup

--- lib/uku_hitbox.dart
+++ lib/uku_hitbox.dart
@@ -1,21 +1,24 @@
 class UkuHitBox {
   final pXDiv6 = 300.0 / 5; // final pXDiv6 = size.width / 5; 
   final pY0Min = 0.0; // 
-  final pY0Max = 22.0; // pYBegin + (strokeWidth * 3) + 20.0
+  final pY0Max = 14.0; // pYBegin + (strokeWidth * 3) + 2.0
   final _espacementY = 40.0;
   final pYBeginLigne1 = 12.0; //pYBegin + (strokeWidth * 3);
 
   // pXDiv6 + (pXDiv6 / 2)
 
-  // 1 => pYBeginLigne1 + _espacementY / 2                        | pYBeginLigne1 + (_espacementY * 1) + _espacementY / 2)
-  // 2 => pYBeginLigne1 + (_espacementY * 2.0) + _espacementY / 2 | pYBeginLigne1 + (_espacementY * 1) + _espacementY / 2)
-  // 3 => pYBeginLigne1 + (_espacementY * 3.0) + _espacementY / 2 | pYBeginLigne1 + (_espacementY * 2) + _espacementY / 2)
+  // 1 => pYBeginLigne1                         | pYBeginLigne1 + (_espacementY * 1) 
+  // 2 => pYBeginLigne1 + (_espacementY * 2.0)  | pYBeginLigne1 + (_espacementY * 1) 
+  // 3 => pYBeginLigne1 + (_espacementY * 3.0)  | pYBeginLigne1 + (_espacementY * 2)
 
   int detectLine(double y) {
     if (y >= pY0Min && y <= pY0Max)
       return 0;
     else
-      return -1;
+      if (y >= pYBeginLigne1 + _espacementY / 2 && y <= pYBeginLigne1 + (_espacementY * 1))
+        return 1;
+      else
+        return -1;
   }
 
   int detectColumn(double x) {
